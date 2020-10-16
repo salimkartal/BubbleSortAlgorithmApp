4a9b3f259f71d4d6ce4635fd46fae6c02e7837c2
Kartal <salimkartal@outlook.com.tr>
2020-10-16 14:58:10 +0300

    first commit

---
+++ WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SAYILAR = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 266);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SIRALA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SAYILAR
+            // 
+            this.SAYILAR.ForeColor = System.Drawing.Color.Red;
+            this.SAYILAR.FormattingEnabled = true;
+            this.SAYILAR.ItemHeight = 18;
+            this.SAYILAR.Location = new System.Drawing.Point(156, 41);
+            this.SAYILAR.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.SAYILAR.Name = "SAYILAR";
+            this.SAYILAR.Size = new System.Drawing.Size(201, 166);
+            this.SAYILAR.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(25, 51);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(92, 36);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.Color.Red;
+            this.textBox2.Location = new System.Drawing.Point(25, 110);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(92, 36);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.ForeColor = System.Drawing.Color.Red;
+            this.textBox3.Location = new System.Drawing.Point(25, 171);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(92, 36);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.ForeColor = System.Drawing.Color.Red;
+            this.textBox4.Location = new System.Drawing.Point(25, 237);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(92, 36);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.ForeColor = System.Drawing.Color.Red;
+            this.textBox5.Location = new System.Drawing.Point(25, 303);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(92, 36);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.ForeColor = System.Drawing.Color.Red;
+            this.textBox6.Location = new System.Drawing.Point(25, 358);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(92, 36);
+            this.textBox6.TabIndex = 12;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication5.Properties.Resources.kabarcık_sıralaması1;
+            this.pictureBox1.Location = new System.Drawing.Point(403, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(298, 298);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 398);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 18);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Yazilar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(713, 512);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.SAYILAR);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "BUBBLE SORT ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox SAYILAR;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int[] Kabarcik_Siralama_Algoritmasi(int[] islenecekDizi)
+        {
+            
+            int gecici = 0;
+            for (int i = 0; i < islenecekDizi.Count(); i++)
+            {
+                
+                for (int j = 0; j < islenecekDizi.Count() - (i + 1); j++)
+                {
+                    if (islenecekDizi[j] > islenecekDizi[(j + 1)])
+                    {
+                        gecici = islenecekDizi[j];
+                        islenecekDizi[j] = islenecekDizi[j + 1];
+                        islenecekDizi[j + 1] = gecici;
+                    }
+
+                    //diziyi yazdırma
+                    foreach (int eleman in islenecekDizi)
+                        Console.Write(eleman + " ");
+
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            return islenecekDizi;
+        }
+
+        int tik=0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tik++;
+            int[] dizi = new int[6];
+            int a = Convert.ToInt32(textBox1.Text);
+            dizi[0] = a;
+            int b = Convert.ToInt32(textBox2.Text);
+            dizi[1] = b;
+            int c = Convert.ToInt32(textBox3.Text);
+            dizi[2] = c;
+            int d = Convert.ToInt32(textBox4.Text);
+            dizi[3] = d;
+            int f = Convert.ToInt32(textBox5.Text);
+            dizi[4] = f;
+            int g = Convert.ToInt32(textBox6.Text);
+            dizi[5] = g;
+
+
+            dizi = Kabarcik_Siralama_Algoritmasi(dizi);
+
+            SAYILAR.Items.Clear();
+            for (int i = 0; i < dizi.Length-4 ; i++)
+            {
+                if (tik == 1)
+                {
+                    SAYILAR.Items.Add(dizi[i]);
+                    label1.Text="Sayi en baştan yanındaki ile karşılaştırılıp ilerliyor";
+                }
+
+            }
+            for (int i = 0; i < dizi.Length-2 ; i++)
+            {
+                if (tik == 2)
+                {
+                    SAYILAR.Items.Add(dizi[i]);
+                    label1.Text = "Sonra dizinin sonuna kadar böyle devam edip \ntekrardan en baştan karşılaştırma yapıyor";
+                }
+
+            }
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (tik == 3)
+                {
+                    SAYILAR.Items.Add(dizi[i]);
+                    label1.Text = "Sonuçta 3 aşamada bu sıralama işlemi tamamlanmış oluyor:\nZaman Karmaşıklığı: n^2 +7 \nBüyük O Notasyonu: best-case: O(n) worst-case: O(n^2)";
+                }
+
+            }
+        }
+    } 
+}
+    
+
